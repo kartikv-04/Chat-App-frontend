978f8df2fa6aeeeec50c9c5860505b352151f0ca
kvr2834 <kartikvaria101@gmail.com>
2025-07-26 15:00:22 +0530

    further update2

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,28 @@
-if (import.meta.env.VITE_APP_MODE === "production") {
-  console.log = () => {};
-  console.warn = () => {};
-  console.error = () => {};
-  console.debug = () => {};
-  console.info = () => {};
-}
+
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+window.addEventListener('error', (e) => {
+  e.stopImmediatePropagation?.();
+  e.preventDefault?.();
+  return false;
+});
+
+window.addEventListener('unhandledrejection', (e) => {
+  e.stopImmediatePropagation?.();
+  e.preventDefault?.();
+  return false;
+});
+
+
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
-      <App />
+  <BrowserRouter>
+    <App />
 
-    </BrowserRouter>
+  </BrowserRouter>
 
 )
