c725a545105bb1877543a99217d47143fbea6476
kvr2834 <kartikvaria101@gmail.com>
2025-07-26 12:15:12 +0530

    url update

--- src/lib/axio.js
+++ src/lib/axio.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_SOCKET_URL=== "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:5000/api"
+    : import.meta.env.VITE_SOCKET_URL + "/api",
   withCredentials: true,
 });
